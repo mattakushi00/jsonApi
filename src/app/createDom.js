335bc5de7886e25f30875dd2d439f7bfb8f8912c
gromov_Max <m.gromov@leadtop.local>
2021-03-11 12:09:19 +0300

    refactoring \refresh tableList after input

--- src/app/createDom.js
+++ src/app/createDom.js
@@ -1,12 +1,17 @@
 export default function (arrayItem, arrayProp) {
 	const $list = document.querySelector('.list')
 	const $filters = document.querySelector('.filters')
+	let $items
 
-	arrayItem.forEach((obj, index) => {
-		$list.insertAdjacentHTML('beforeend', `<div class="list__item"></div>`)
+	arrayProp.forEach((prop, index) => {
 		$filters.insertAdjacentHTML('beforeend', `<input type="text" placeholder="${arrayProp[index].slice(4)}" name="${arrayProp[index]}" class="filters__item">`)
 	})
-	const $items = document.querySelectorAll('.list__item')
+
+	arrayItem.forEach(() => {
+		$list.insertAdjacentHTML('beforeend', `<div class="list__item"></div>`)
+	})
+
+	$items = document.querySelectorAll('.list__item')
 
 	$items.forEach((item, index) => {
 		arrayProp.forEach(prop => {
--- src/app/filters.js
+++ src/app/filters.js
@@ -3,7 +3,6 @@ export default function (object, arrayProp) {
 	const $filters__item = document.querySelectorAll('.filters__item')
 	const $list__item = document.querySelectorAll('.list__item')
 	const $buttonClear = document.querySelector('.filters__btn_clear')
-	const $buttonSearch = document.querySelector('.filters__btn_search')
 
 	$buttonClear.addEventListener('click', (e) => {
 		e.preventDefault()
@@ -15,26 +14,20 @@ export default function (object, arrayProp) {
 		})
 		$list__item.forEach(item => item.classList.remove('display__none'))
 	})
-
+	console.log(object)
 	$filters.addEventListener('input', (e) => {
 		if (e.target.classList.contains('filters__item')) {
-			object[e.target.name] = e.target.value
-		}
-	})
-
-	$buttonSearch.addEventListener('click', (e) => {
-		e.preventDefault()
-		$list__item.forEach((item, index__item) => {
-			item.classList.remove('display__none')
-			item.childNodes.forEach((prop, index__prop) => {
-				if (
-					(object[index__item][arrayProp[index__prop]].$t.indexOf($filters__item[index__prop].value) === -1) &&
-					($filters__item[index__prop].value.length !== 0)
-				) {
-					console.log('lal');
-					item.classList.add('display__none')
-				}
+			$list__item.forEach((item, index__item) => {
+				item.classList.remove('display__none')
+				item.childNodes.forEach((prop, index__prop) => {
+					if (
+						(object[index__item][arrayProp[index__prop]].$t.indexOf($filters__item[index__prop].value) === -1) &&
+						($filters__item[index__prop].value.length !== 0)
+					) {
+						item.classList.add('display__none')
+					}
+				})
 			})
-		})
+		}
 	})
 }
