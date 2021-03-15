8eee961b8a0afc4c5bd9c28035992ada3670628e
gromov_Max <m.gromov@leadtop.local>
2021-03-15 12:38:37 +0300

    WIP

--- src/app/sheetsApi.js
+++ src/app/sheetsApi.js
@@ -5,7 +5,7 @@ var $YOURCODE = your code from url (between spreadsheets/d/ and /edit#gid=0) }
 export default function() {
 	const listSheets = {
 		testSheets: '1hk5iB5KF-3rNLVknOKayWkQtWVZIu2gZcmBo08hXpcM',
-		yourCode: '1A-Go8MVblUB7NXeErhvY7ELKtPUnI0KgolXPfiO0mWA',
+		yourCode: '1t2TS4zKggDdL6qcVlMllATW74t-ycpvIlB_NOw2t_Ww',
 	}
 
 	return fetch(`https://spreadsheets.google.com/feeds/list/${listSheets.testSheets}/od6/public/values?alt=json`)
